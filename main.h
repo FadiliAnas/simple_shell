0ce4524c9357b9a184d62f431bcc2fed222022a0
Aymane <aymane.lamarti.8@gmail.com>
2023-12-10 21:01:31 +0000

    simple shell

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H 
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_TOKENS 100
+
+int main(void);
+char *str_concat(char *s1, char *s2);
+char *path(char *command, char **env);
+int check_fullpath(char *token);
+
+
+#endif
--- super_simple_shell.c
+++ super_simple_shell.c
@@ -1,18 +1,65 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <string.h>
-#define MAX_TOKENS 100
+#include "main.h"
 
-int main (void)
+
+char *path(char *command, char **env)
 {
+	int i, j;
+	char *tok, *path_token, *commands_full_path, *env_val;
+	char *full_paths[1024];
+
+	for (i = 0; env[i] != NULL; i++)
+	{
+		tok = strtok(env[i], "=");
+		if (strcmp(tok, "PATH"))
+		{
+			env_val = strtok(NULL, "\n");
+			path_token = strtok(env_val, ":");
+			j = 0;
+			while (path_token != NULL)
+			{
+				full_paths[j] = path_token;
+				path_token = strtok(NULL, "\n");
+				j++;
+			}
+			full_paths[j] = NULL;
+			j = 0;
+			while (full_paths[j])
+			{
+				commands_full_path = str_concat(full_paths[j], command);
+				if (access(commands_full_path, X_OK) == 0)
+					return (commands_full_path);
+				j++;
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+int check_fullpath(char *token)
+{
+	int i;
+
+	for (i = 0; (token + i) != NULL ; i++)
+	{
+		if (token[i] == '/')
+			return (1);
+	}
+	return (0);
+}
+
+
+
+int main(void)
+{
+	extern char **environ;
 	char *buffer = 0;
 	size_t buffer_size;
 	int x, i;
-	/*int counter = 0;*/
 	char *token;
 	char *tokens[MAX_TOKENS];
+	char *_path;
+
 
 	while(1)
 	{
@@ -29,14 +76,27 @@ int main (void)
 			i++;
 		}
 		tokens[i] = NULL;
-		x = fork();
-		/*while(buffer[counter] != '\n')
-			counter++;
-		buffer[counter] = '\0';*/
-		if(x != 0)
-			wait(0);
+
+
+		if (!check_fullpath(tokens[0]))
+		{
+			_path = path(tokens[0], environ);
+			if (_path == NULL)
+				perror("not found");
+			x = fork();
+			if (x != 0)
+				wait (0);
+			else
+				execve(_path, tokens, environ);
+		}
 		else
-			execve(tokens[0], tokens, 0);
+		{
+			x = fork();
+			if(x != 0)
+				wait(0);
+			else
+				execve(tokens[0], tokens, environ);
+		}
 	}
 	return (0);
 }
